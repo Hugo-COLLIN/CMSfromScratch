4f1a0847c99bf401156b9e0615e50f33778aa3f3
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-12 19:15:03 +0200

    form field validation works

--- admin/index.php
+++ admin/index.php
@@ -7,10 +7,19 @@ include_once ('../includes/connection.php');
 if (isset($_SESSION['logged_in']))
 {
     //display index
+
 }
 else
 {
     //display login
+    if (isset($_POST['username'], $_POST['password']))
+    {
+        $uname = $_POST['username'];
+        $pwd = $_POST['password'];
+
+        if (empty($uname) OR empty($pwd))
+            $error = "All fields are required !";
+    }
     ?>
 
     <!doctype html>
@@ -21,12 +30,23 @@ else
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>CMS Tutorial</title>
-        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="../assets/style.css">
     </head>
     <body>
     <div class="container">
-        <a href="index.php" id="logo">CMS</a>
+        <h2><a href="index.php" id="logo">CMS</a></h2>
 
+        <?php if (isset($error)) { ?>
+            <small id="errMsg"><?php echo $error; ?></small>
+            <br><br>
+        <?php } ?>
+
+        <form id="formLogin" action="./" method="post" autocomplete="off">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Login">
+
+        </form>
 
     </div>
     </body>
